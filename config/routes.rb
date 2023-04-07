5753dbdfc3d06aa3e856e8157c1efbafaa6e5dfb
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-04-07 09:18:15 +0000

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  
+
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update]
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
